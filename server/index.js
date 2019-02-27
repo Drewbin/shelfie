5d81c4907b05d6df0b45f030fb65c86ad45977bb
Andrew Johnson <drewbey@gmail.com>
2019-02-27 02:46:54 -0700

    Server set up

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+
+
+
+
+app.listen(3003, () => {
+    console.log('Server is running on port 3003.')
+})
